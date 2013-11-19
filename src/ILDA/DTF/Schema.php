bab204b15f54ac4c9e170b479656d1505826702f
Damien Walsh <me@damow.net>
2013-11-19 23:09:58 +0000

    Made the schema format more generalised.
     * Now Schema ignores all JSON fields not prefixed with an _ and passes them to the created FieldInterface-implementing objects.

--- src/ILDA/DTF/Schema.php
+++ src/ILDA/DTF/Schema.php
@@ -7,6 +7,8 @@
  */
 namespace ILDA\DTF;
 
+use ILDA\DTF\Fields\CompoundField;
+
 /**
  * Schema
  * Represents the internal structure of a binary field file.
@@ -21,22 +23,61 @@ class Schema
     public $fields = array();
 
     /**
-     * Initialise a new schema with an array of fields.
+     * Initialise a new schema with a definition in the form of an array of fields.
      *
-     * @param array $fields The field set to initialise the schema with.
+     * @param array $definition The field set to initialise the schema with.
+     * @param Fields\CompoundField $parent The CompoundField, if any, to populate with the schema.
      */
-    public function initWithArray(array $fields)
+    public function initWithSchemaDefinition(array $definition)
     {
-        $this->fields = $fields;
+        foreach ($definition as $fieldName => $field) {
+            $this->addDefinedField($fieldName, $field);
+        }
+
+        return $this;
+    }
+
+    private function addDefinedField($fieldName, array $definition, CompoundField $targetField = null)
+    {
+        $className = '\\ILDA\\DTF\\Fields\\' . $definition['_type'];
+        $newField = new $className;
+
+        // Set the properties on the field
+        foreach ($definition as $propertyName => $propertyValue) {
+            if ($propertyName[0] === '_') {
+                // Don't add special-meaning _ fields
+                continue;
+            }
+
+            $newField->{$propertyName} = $propertyValue;
+        }
+
+        // Are we adding a compound field?
+        if (is_a($newField, '\\ILDA\\DTF\\Fields\\CompoundField')) {
+            if (isset($definition['_fields'])) {
+                // Adding a compound field that has some subfields
+                foreach ($definition['_fields'] as $subFieldName => $subFieldDefinition) {
+                    $this->addDefinedField($subFieldName, $subFieldDefinition, $newField);
+                }
+            }
+        }
+
+        if ($targetField) {
+            // Adding the field to an existing compound field
+            $targetField->addField($fieldName, $newField);
+        } else {
+            // Adding the field to this schema
+            $this->addField($fieldName, $newField);
+        }
     }
 
     /**
      * @param AbstractField $field The field to add to the schema.
      * @return $this
      */
-    public function addField(Fields\AbstractField $field)
+    public function addField($fieldName, Fields\FieldInterface $field)
     {
-        $this->fields[] = $field;
+        $this->fields[$fieldName] = $field;
         return $this;
     }
 
@@ -48,8 +89,8 @@ class Schema
     {
         $readFields = array();
 
-        foreach ($this->fields as $field) {
-            $readFields[] = $field->read($stream);
+        foreach ($this->fields as $fieldName => $field) {
+            $readFields[$fieldName] = $field->read($stream);
         }
 
         return $readFields;
